2b57606434eb60dd7b5d2a58e06f9b679774ec0e
anjucsaji <71403833+anjucsaji@users.noreply.github.com>
2020-10-07 13:59:01 -0400

    Add project files.

---
+++ Assignment_1_Triangle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_1_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
